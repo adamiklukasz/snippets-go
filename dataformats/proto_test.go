a402e3444598741511598e50b69507283fc2b6c5
Adamik Lukasz <adamik.lukasz.1985@gmail.com>
2020-04-26 00:14:04 +0200

    Protocol buffer load/save

---
+++ dataformats/proto_test.go
@@ -0,0 +1,34 @@
+package dataformats
+
+import (
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"io/ioutil"
+	"snippets-go/dataformats/defs"
+	"testing"
+)
+
+func TestProtobufSave(t *testing.T) {
+	p := defs.Person{
+		Name:  "Lukasz",
+		Id:    21,
+		Email: "lukasz.1234@oa.qw",
+		Phones: []*defs.Person_PhoneNumber{
+			{
+				Number: "123-456-789",
+				Type:   defs.Person_HOME,
+			},
+		},
+	}
+
+	out, _ := proto.Marshal(&p)
+	_ = ioutil.WriteFile("person.sav", out, 0644)
+}
+
+func TestProtobufLoad(t *testing.T) {
+	in, _ := ioutil.ReadFile("person.sav")
+	p := defs.Person{}
+	_ = proto.Unmarshal(in, &p)
+
+	fmt.Printf("person=[%v]\n", p)
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.14
 require (
 	github.com/Pallinder/go-randomdata v1.2.0
 	github.com/bshuster-repo/logrus-logstash-hook v0.4.1
+	github.com/golang/protobuf v1.4.0
 	github.com/sirupsen/logrus v1.5.0
 	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/protobuf v1.21.0
 )
