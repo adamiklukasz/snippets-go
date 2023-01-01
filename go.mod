4da9de8fe3aa631afb66a9362bc6bebc823b49c5
Adamik Lukasz <adamik.lukasz.1985@gmail.com>
2023-01-02 00:53:50 +0100

    Generic modified

--- go.mod
+++ go.mod
@@ -1,13 +1,14 @@
-module github.com/adamiklukasz/snippets-go
+module snippets-go
 
-go 1.18
+go 1.19
 
 require (
 	github.com/Pallinder/go-randomdata v1.2.0
 	github.com/sirupsen/logrus v1.9.0
+	golang.org/x/exp v0.0.0-20221230185412-738e83a70c30
 )
 
 require (
-	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	golang.org/x/sys v0.1.0 // indirect
 	golang.org/x/text v0.5.0 // indirect
 )
--- language/generics/main.go
+++ language/generics/main.go
@@ -2,14 +2,8 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/exp/constraints"
 )
-
-func main() {
-	a := Max([]int{2, 4, 5, 6, 8})
-	b := Max([]float64{4, 11, 34.5, 34})
-	fmt.Printf("a=%#v\n", a)
-	fmt.Printf("b=%#v\n", b)
-}
 
 func Max[T int | float64](arr []T) T {
 	var max T = 0
@@ -20,4 +14,23 @@ func Max[T int | float64](arr []T) T {
 	}
 
 	return max
+}
+
+func SumGeneric[K comparable, V constraints.Integer | constraints.Float](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+func main() {
+	a := Max([]int{2, 4, 5, 6, 8})
+	b := Max([]float64{4, 11, 34.5, 34})
+	fmt.Printf("a=%#v\n", a)
+	fmt.Printf("b=%#v\n", b)
+
+	c := map[string]float64{"a": 31.2, "b": 12.9}
+	r := SumGeneric(c)
+	fmt.Printf("r=%#v\n", r)
 }
